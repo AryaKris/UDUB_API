cb08ee6151140ba38b233738cd43cd4733c8fa06
Arya Krishna <dr.aryakris@outlook.com>
2024-02-29 10:31:48 -0800

    Add files via upload

---
+++ Day-01/02-Stu_Window-Object/Unsolved/script.js
@@ -0,0 +1,12 @@
+// TODO: What will the following line of code log? the whole browser? 
+console.log(window);
+
+// TODO: What will the following line of code log? the whole html document
+console.log(window.document);
+
+// TODO: What will the following line of code log? all the elements 
+console.log(document.documentElement);
+
+// TODO: What will the following line of code log? head elementt 
+console.log(document.head);
+
